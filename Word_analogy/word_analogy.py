69103d7ca6127a48d5e2f4861829657447820110
Masher828 <Manish.cse828@gmail.com>
2020-04-25 00:56:37 +0530

    GUI Updated

--- Word_analogy/word_analogy.py
+++ Word_analogy/word_analogy.py
@@ -7,12 +7,22 @@ def word_analogyy(dict,words):
     wb=dict[words[1]]
     wc=dict[words[2]]
     max_sim=-100
-    for w in dict.keys():
-        if w in words:
-            continue
-        wd=dict[w]
-        sim=cosine_similarity([wb-wa],[wd-wc])
-        if sim>max_sim:
-            max_sim=sim
-            d=w
+    try:
+        for w in dict.vocab.keys():
+            if w in words:
+                continue
+            wv = dict[w]
+            sim = cosine_similarity([wb-wa],[wv-wc])
+            if sim > max_sim:
+                max_sim = sim
+                d=w
+    except :
+        for w in dict.keys():
+            if w in words:
+                continue
+            wd=dict[w]
+            sim=cosine_similarity([wb-wa],[wd-wc])
+            if sim>max_sim:
+                max_sim=sim
+                d=w
     return d
